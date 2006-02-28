5f976bd2fddc8d30f723a5fc3428e2d7362fba83
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2006-02-28 17:49:24 +0000

    Put into namespace stor, and added FU sender registration and statistics.

--- test/testStorageManager.h
+++ test/testStorageManager.h
@@ -52,67 +52,84 @@
 
 using namespace std;
 
-class testStorageManager: public xdaq::Application
-{
- public:
-  //XDAQ_INSTANTIATOR();
-  testStorageManager(xdaq::ApplicationStub* s) throw (xdaq::exception::Exception);
+namespace stor {
 
-  ~testStorageManager();
+  struct SMFUSenderList;
 
- private:
-  void configureAction(toolbox::Event::Reference e) throw (toolbox::fsm::exception::Exception);
-  void enableAction(toolbox::Event::Reference e) throw (toolbox::fsm::exception::Exception);
-  virtual void haltAction(toolbox::Event::Reference e) 
-    throw (toolbox::fsm::exception::Exception);
-  virtual void suspendAction(toolbox::Event::Reference e) 
-    throw (toolbox::fsm::exception::Exception);
-  virtual void resumeAction(toolbox::Event::Reference e) 
-    throw (toolbox::fsm::exception::Exception);
-  virtual void nullAction(toolbox::Event::Reference e) 
-    throw (toolbox::fsm::exception::Exception);
-  xoap::MessageReference fireEvent(xoap::MessageReference msg)
-    throw (xoap::exception::Exception);
+  class testStorageManager: public xdaq::Application
+  {
+   public:
+    //XDAQ_INSTANTIATOR();
+    testStorageManager(xdaq::ApplicationStub* s) throw (xdaq::exception::Exception);
+  
+    ~testStorageManager();
+  
+   private:
+    void configureAction(toolbox::Event::Reference e) throw (toolbox::fsm::exception::Exception);
+    void enableAction(toolbox::Event::Reference e) throw (toolbox::fsm::exception::Exception);
+    virtual void haltAction(toolbox::Event::Reference e) 
+      throw (toolbox::fsm::exception::Exception);
+    virtual void suspendAction(toolbox::Event::Reference e) 
+      throw (toolbox::fsm::exception::Exception);
+    virtual void resumeAction(toolbox::Event::Reference e) 
+      throw (toolbox::fsm::exception::Exception);
+    virtual void nullAction(toolbox::Event::Reference e) 
+      throw (toolbox::fsm::exception::Exception);
+    xoap::MessageReference fireEvent(xoap::MessageReference msg)
+      throw (xoap::exception::Exception);
+  
+    void receiveRegistryMessage(toolbox::mem::Reference *ref);
+    void receiveDataMessage(toolbox::mem::Reference *ref);
+    void receiveOtherMessage(toolbox::mem::Reference *ref);
 
-  void receiveRegistryMessage(toolbox::mem::Reference *ref);
-  void receiveDataMessage(toolbox::mem::Reference *ref);
-  void receiveOtherMessage(toolbox::mem::Reference *ref);
+    void registerFUSender(const char* hltURL, const char* hltClassName,
+                 const unsigned long hltLocalId, const unsigned long hltInstance,
+                 const unsigned long hltTid,
+                 const unsigned long registrySize, const char* registryData);
+    void updateFUSender4data(const char* hltURL,
+      const char* hltClassName, const unsigned long hltLocalId,
+      const unsigned long hltInstance, const unsigned long hltTid,
+      const unsigned long runNumber, const unsigned long eventNumber,
+      const unsigned long frameNum, const unsigned long totalFrames,
+      const unsigned long origdatasize, const bool isLocal);
+  
+    void defaultWebPage
+      (xgi::Input *in, xgi::Output *out) throw (xgi::exception::Exception);
+    void css(xgi::Input *in, xgi::Output *out) throw (xgi::exception::Exception)
+      {css_.css(in,out);}
+  
+    evf::EPStateMachine *fsm_;
+    //edm::AssertHandler *ah_;
+    edm::service::MessageServicePresence theMessageServicePresence;
+    xdata::String offConfig_;
+    xdata::String fuConfig_;
+    friend class evf::EPStateMachine;
+  
+    boost::shared_ptr<stor::JobController> jc_;
+  
+    evf::Css css_;
+    unsigned long eventcounter_;
+    unsigned long framecounter_;
+    int pool_is_set_;
+    toolbox::mem::Pool *pool_;
 
-  void defaultWebPage
-    (xgi::Input *in, xgi::Output *out) throw (xgi::exception::Exception);
-  void css(xgi::Input *in, xgi::Output *out) throw (xgi::exception::Exception)
-    {css_.css(in,out);}
-
-  evf::EPStateMachine *fsm_;
-  //edm::AssertHandler *ah_;
-  edm::service::MessageServicePresence theMessageServicePresence;
-  xdata::String offConfig_;
-  xdata::String fuConfig_;
-  friend class evf::EPStateMachine;
-
-  boost::shared_ptr<stor::JobController> jc_;
-
-  evf::Css css_;
-  unsigned long eventcounter_;
-  unsigned long framecounter_;
-  int pool_is_set_;
-  toolbox::mem::Pool *pool_;
-
-  // for performance measurements
-  void addMeasurement(unsigned long size);
-  xdata::UnsignedLong samples_; //number of samples (frames) per measurement
-  stor::SMPerformanceMeter *pmeter_;
-  // measurements for last set of samples
-  xdata::Double databw_;      // bandwidth in MB/s
-  xdata::Double datarate_;    // number of frames/s
-  xdata::Double datalatency_; // micro-seconds/frame
-  xdata::UnsignedLong totalsamples_; //number of samples (frames) per measurement
-  xdata::Double duration_;        // time for run in seconds
-  xdata::Double meandatabw_;      // bandwidth in MB/s
-  xdata::Double meandatarate_;    // number of frames/s
-  xdata::Double meandatalatency_; // micro-seconds/frame
-  xdata::Double maxdatabw_;       // maximum bandwidth in MB/s
-  xdata::Double mindatabw_;       // minimum bandwidth in MB/s
-
-};
+    vector<SMFUSenderList> smfusenders_;
+  
+    // for performance measurements
+    void addMeasurement(unsigned long size);
+    xdata::UnsignedLong samples_; //number of samples (frames) per measurement
+    stor::SMPerformanceMeter *pmeter_;
+    // measurements for last set of samples
+    xdata::Double databw_;      // bandwidth in MB/s
+    xdata::Double datarate_;    // number of frames/s
+    xdata::Double datalatency_; // micro-seconds/frame
+    xdata::UnsignedLong totalsamples_; //number of samples (frames) per measurement
+    xdata::Double duration_;        // time for run in seconds
+    xdata::Double meandatabw_;      // bandwidth in MB/s
+    xdata::Double meandatarate_;    // number of frames/s
+    xdata::Double meandatalatency_; // micro-seconds/frame
+    xdata::Double maxdatabw_;       // maximum bandwidth in MB/s
+    xdata::Double mindatabw_;       // minimum bandwidth in MB/s
+  }; // end of class
+} // end of namespace stor
 #endif
