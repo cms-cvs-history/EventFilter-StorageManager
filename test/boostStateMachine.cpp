f025e69d827db8489defb61c946e0258770c83ca
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2008-12-23 19:17:56 +0000

    Updated state machine example to demonstrate use of free functions; added many comments

--- test/boostStateMachine.cpp
+++ test/boostStateMachine.cpp
@@ -8,14 +8,15 @@
 namespace sc = boost::statechart;
 
 
-// Forward definition of the states
+// Forward definition of the states and base class.
+class SMOperations;
 class Normal;
 class Failed;
 class Halted;
 class Ready;
 class Enabled;
 
-// Definition of the state transitions
+// Definition of the events.
 class Configure : public sc::event< Configure > {};
 class Enable    : public sc::event< Enable > {};
 class Stop      : public sc::event< Stop > {};
@@ -26,27 +27,55 @@ class SMOperations
 {
 public:
 
-    virtual void handleI2OEventMessage() const
-    {
-        std::cout << "ERROR: handleI2OEventMessage not supported in "
-                  << stateName_ << " state!" << std::endl;
-    }
+  // SMOperations is a base class, so it should have a virtual
+  // destructor. Furthermore, it should be an abstract class; one
+  // should not make instances of type SMOperations. Thus it is best
+  // to have it be a pure interface, containing only abstract
+  // functions and no data.
 
-protected:
-
-    std::string stateName_;
+  virtual ~SMOperations() = 0;
+  virtual void handleI2OEventMessage() const = 0;
+  virtual const char* stateName() const = 0;
 };
 
-// Define the state machine with its initial state
+// Even though the destructor is pure virtual, we still need an
+// implementation.
+SMOperations::~SMOperations()
+{
+  // nothing to do.
+}
+
+
+// These are examples of the I2O message handling functions. This is
+// not a full set, nor are the implementations at all interesting.
+namespace I2OMessageHandlingFunctions
+{
+  void logError(SMOperations const& me)
+  {
+    std::cout << "ERROR: illegal call made while in state "
+	      << me.stateName() << std::endl;
+  }
+
+  void logSuccess(SMOperations const& me)
+  {
+    std::cout << "SUCCESS: legal call made while in state "
+	      << me.stateName() << std::endl;
+  }
+}
+
+
+// Define the state machine with its initial state.
 class SMStateMachine : public sc::state_machine< SMStateMachine, Normal >
 {
 public:
 
-    void handleI2OEventMessage()
-    {
-        const SMOperations *ptr = state_cast< const SMOperations * >();
-        ptr->handleI2OEventMessage();
-    }
+  void handleI2OEventMessage()
+  {
+    //  Use the reference form to throw an exception in case of a
+    //  failed cast.
+    SMOperations const& ref = state_cast<SMOperations const&>();
+    ref.handleI2OEventMessage();
+  }
 };
 
 // The NORMAL state is the default outer state of the SMStateMachine.
@@ -56,21 +85,26 @@ class Normal : public sc::simple_state< Normal, SMStateMachine, Halted >,
 {
 public:
 
-    // The NORMAL state can only fail
-    typedef boost::mpl::list<
+  // The NORMAL state can only fail
+  typedef boost::mpl::list<
     sc::transition< Fail, Failed > 
-    > reactions;
+  > reactions;
 
-    Normal() 
-    { 
-        stateName_ = "Normal";
-        std::cout << "Entering '" << stateName_ << "' outer state" << std::endl;
-    }
+  Normal() 
+  { 
+    std::cout << "Entering '" << stateName() << "' outer state" << std::endl;
+  }
     
-    ~Normal() 
-    { 
-        std::cout << "Exiting 'NORMAL' outer state" << std::endl;
-    }
+  virtual ~Normal() 
+  { 
+    std::cout << "Exiting 'NORMAL' outer state" << std::endl;
+  }
+
+  virtual const char* stateName() const { return "Normal"; }
+  virtual void handleI2OEventMessage () const 
+  {
+    I2OMessageHandlingFunctions::logError(*this);
+  }
 };
 
 // The FAILED state is an outer state
@@ -79,18 +113,24 @@ class Failed : public sc::simple_state< Failed, SMStateMachine >,
 {
 public:
 
-    // There's no way out of the FAILED state.
+  // There's no way out of the FAILED state.
 
-    Failed() 
-    { 
-        stateName_ = "Failed";
-        std::cout << "Entering 'FAILED' outer state" << std::endl;
-    }
+  Failed() 
+  { 
+    std::cout << "Entering 'FAILED' outer state" << std::endl;
+  }
     
-    ~Failed() 
-    { 
-        std::cout << "Exiting 'FAILED' outer state" << std::endl;
-    }
+  virtual ~Failed() 
+  { 
+    std::cout << "Exiting 'FAILED' outer state" << std::endl;
+  }
+
+  virtual const char* stateName() const { return "Failed"; }
+
+  virtual void handleI2OEventMessage () const 
+  {
+    I2OMessageHandlingFunctions::logError(*this);
+  }
 };
 
 
@@ -100,20 +140,26 @@ class Halted : public sc::simple_state< Halted, Normal >,
 {
 public:
 
-    typedef boost::mpl::list<
+  typedef boost::mpl::list<
     sc::transition< Configure, Ready > 
-    > reactions;
+  > reactions;
 
-    Halted() 
-    { 
-        stateName_ = "Halted";
-        std::cout << "    Entering 'HALTED' inner state" << std::endl;
-    }
+  Halted() 
+  { 
+    std::cout << "    Entering 'HALTED' inner state" << std::endl;
+  }
     
-    ~Halted() 
-    { 
-        std::cout << "    Exiting 'HALTED' inner state" << std::endl;
-    }
+  virtual ~Halted() 
+  { 
+    std::cout << "    Exiting 'HALTED' inner state" << std::endl;
+  }
+
+  virtual const char* stateName() const { return "Halted"; }
+
+  virtual void handleI2OEventMessage() const 
+  {
+    I2OMessageHandlingFunctions::logError(*this);
+  }
 };
 
 
@@ -124,21 +170,27 @@ class Ready : public sc::simple_state< Ready, Normal >,
 public:
 
 
-    typedef boost::mpl::list<
+  typedef boost::mpl::list<
     sc::transition< Enable, Enabled >,
     sc::transition< Halt, Halted > 
-    > reactions;
+  > reactions;
 
-    Ready() 
-    { 
-        stateName_ = "Ready";
-        std::cout << "    Entering 'READY' inner state" << std::endl;
-    }
+  Ready() 
+  { 
+    std::cout << "    Entering 'READY' inner state" << std::endl;
+  }
 
-    ~Ready() 
-    { 
-        std::cout << "    Exiting 'READY' inner state" << std::endl;
-    }
+  virtual ~Ready() 
+  { 
+    std::cout << "    Exiting 'READY' inner state" << std::endl;
+  }
+
+  virtual const char* stateName() const { return "Ready"; }
+
+  virtual void handleI2OEventMessage() const 
+  { 
+    I2OMessageHandlingFunctions::logError(*this); 
+  }
 };
 
 
@@ -148,40 +200,41 @@ class Enabled : public sc::state< Enabled, Normal >,
 {
 public:
 
-    typedef boost::mpl::list<
+  typedef boost::mpl::list<
     sc::transition< Stop, Ready >,
     sc::transition< Halt, Halted > 
-    > reactions;
+  > reactions;
 
-    Enabled(my_context ctx) : my_base(ctx)
-    { 
-        ++creationCounter_;
-        stateName_ = "Enabled";
-        if (creationCounter_ >= 3) {
-            std::cout << "  'ENABLED' entry action encountered an error"
-                      << std::endl;
-            post_event( Fail() );
-            return;
-        }
-        std::cout << "    Entering 'ENABLED' inner state" << std::endl;
+  Enabled(my_context ctx) : my_base(ctx)
+  { 
+    ++creationCounter_;
+    if (creationCounter_ >= 3) {
+      std::cout << "  'ENABLED' entry action encountered an error"
+		<< std::endl;
+      post_event( Fail() );
+      return;
     }
+    std::cout << "    Entering 'ENABLED' inner state" << std::endl;
+  }
 
-    ~Enabled() 
-    { 
-        std::cout << "  Closing files gracefully" << std::endl;
-        std::cout << "    Exiting 'ENABLED' inner state" << std::endl;
-    }
+  ~Enabled() 
+  { 
+    std::cout << "  Closing files gracefully" << std::endl;
+    std::cout << "    Exiting 'ENABLED' inner state" << std::endl;
+  }
 
-    void handleI2OEventMessage() const
-    {
-        std::cout << "SUCCESS: handleI2OEventMessage correctly called in "
-                  << stateName_ << " state!" << std::endl;
-    }
+  const char* stateName() const { return "Enabled"; }
+
+
+  virtual void handleI2OEventMessage() const 
+  {
+    I2OMessageHandlingFunctions::logSuccess(*this); 
+  }
 
 private:
 
-    // simple-minded way to get the entry action to fail after N entries
-    static int creationCounter_;
+  // simple-minded way to get the entry action to fail after N entries
+  static int creationCounter_;
 };
 
 int Enabled::creationCounter_ = 0;
@@ -190,41 +243,41 @@ int Enabled::creationCounter_ = 0;
 
 int main()
 {
-    SMStateMachine stateMachine;
+  SMStateMachine stateMachine;
 
-    stateMachine.initiate();
+  stateMachine.initiate();
 
-    std::string transitionList[4] = {"Configure", "Enable",
-                                     "Stop", "Halt"};
+  std::string transitionList[4] = {"Configure", "Enable",
+				   "Stop", "Halt"};
 
-    for (int jdx = 0; jdx < 3; ++jdx) {
-        for (int idx = 0; idx < 4; ++idx) {
-            std::string requestedTransition = transitionList[idx];
+  for (int jdx = 0; jdx < 3; ++jdx) {
+    for (int idx = 0; idx < 4; ++idx) {
+      std::string requestedTransition = transitionList[idx];
 
-            // go directly from Enabled to Halted in one case
-            if (jdx == 1 && idx == 2) {continue;}
+      // go directly from Enabled to Halted in one case
+      if (jdx == 1 && idx == 2) {continue;}
 
-            // this code would look like what would be in
-            // the SM SOAP receiving method, correct?
-            if (requestedTransition == "Configure") {
-                stateMachine.process_event( Configure() );
-            }
-            else if (requestedTransition == "Enable") {
-                stateMachine.process_event( Enable() );
-            }
-            else if (requestedTransition == "Stop") {
-                stateMachine.process_event( Stop() );
-            }
-            else if (requestedTransition == "Halt") {
-                stateMachine.process_event( Halt() );
-            }
+      // this code would look like what would be in
+      // the SM SOAP receiving method, correct?
+      if (requestedTransition == "Configure") {
+	stateMachine.process_event( Configure() );
+      }
+      else if (requestedTransition == "Enable") {
+	stateMachine.process_event( Enable() );
+      }
+      else if (requestedTransition == "Stop") {
+	stateMachine.process_event( Stop() );
+      }
+      else if (requestedTransition == "Halt") {
+	stateMachine.process_event( Halt() );
+      }
 
-            // simulate an event fragment being received
-            // while in a particular state
-            stateMachine.handleI2OEventMessage();
-            ::sleep(1);
-        }
+      // simulate an event fragment being received
+      // while in a particular state
+      stateMachine.handleI2OEventMessage();
+      //::sleep(1);
     }
+  }
 
-    return 0;
+  return 0;
 }
