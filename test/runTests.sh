df92d20380ecc5116858fd2cbe59a7e49597529a
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2009-01-09 18:06:42 +0000

    Return a success/failure code

--- test/runTests.sh
+++ test/runTests.sh
@@ -2,15 +2,19 @@
 
 tests="fsmSoap/fsmSoap.tcl"
 
+error=0
+
 for test in $tests ; do
     startTime=`date "+%H:%M:%S"`
     printf "%-32s: $startTime " $test
     
-#    $test > $test.log 2>&1
     if $test > $test.log 2>&1
 	then
 	date "+%H:%M:%S Passed"
     else
 	date "+%H:%M:%S FAILED"
+	error=1
     fi
 done
+
+exit $error
