7e4bba22b106058013b91e222bb63ad8f7ab26a7
Kurt Biery <sha1-a0b81a63ebfa22b3eb476206725e29d6b356d7db@cern.ch>
2009-01-29 22:13:51 +0000

    Updated the MonitorQuantity class based on our latest discussion - still needs tested.

--- interface/MonitoredQuantity.h
+++ interface/MonitoredQuantity.h
@@ -1,7 +1,11 @@
-// $Id: MonitoredQuantity.h,v 1.1 2008/04/14 15:42:28 biery Exp $
+// $Id: MonitoredQuantity.h,v 1.1.2.1 2009/01/29 17:17:14 mommsen Exp $
 
 #ifndef StorageManager_MonitoredQuantity_h
 #define StorageManager_MonitoredQuantity_h
+
+#include "boost/shared_ptr.hpp"
+#include "boost/thread/mutex.hpp"
+#include <vector>
 
 namespace stor
 {
@@ -10,9 +14,9 @@ namespace stor
    * This class keeps track of statistics for a set of sample values 
    * and provides timing information on the samples.
    *
-   * \author $Author: $
-   * \version $Revision: $
-   * \date $Date: $
+   * \author $Author: mommsen $
+   * \version $Revision: 1.1.2.1 $
+   * \date $Date: 2009/01/29 17:17:14 $
    *
    */
 
@@ -21,62 +25,173 @@ namespace stor
     
   public:
 
+    static const int EXPECTED_CALCULATION_INTERVAL = 1;  // seconds
+
     enum DataSetType { FULL = 0,      // the full data set (all samples)
                        RECENT = 1 };  // recent data only
 
-    MonitoredQuantity();
+    MonitoredQuantity(double timeWindowForRecentResults = 10.0);
 
+    /**
+     * Adds the specified sample value to the monitor instance.
+     */
     void addSample(double value = 1.0);
-    long long getSampleCount(DataSetType dataSet = FULL) const;
-    double getSampleRate(DataSetType dataSet = FULL,
-                         double currentTime = getCurrentTime()) const;
-    double getValueSum(DataSetType dataSet = FULL) const;
-    double getValueAverage(DataSetType dataSet = FULL) const;
-    double getValueRMS(DataSetType dataSet = FULL) const;
-    double getValueMin(DataSetType dataSet = FULL) const;
-    double getValueMax(DataSetType dataSet = FULL) const;
-    double getValueRate(DataSetType dataSet = FULL,
-                        double currentTime = getCurrentTime()) const;
-    double getDuration(DataSetType dataSet = FULL,
-                       double currentTime = getCurrentTime()) const;
 
-    void latchRecentValuesIfNeeded(double currentTime = getCurrentTime());
+    /**
+     * Returns the number of samples stored in the monitor.
+     */
+    long long getSampleCount(DataSetType dataSet = FULL);
+
+    /**
+     * Returns the rate of samples stored in the monitor
+     * (number of samples divided by duration).  The units of the
+     * return value are samples per second.
+     */
+    double getSampleRate(DataSetType dataSet = FULL);
+
+    /**
+     * Returns the sum of all sample values stored in the monitor.
+     */
+    double getValueSum(DataSetType dataSet = FULL);
+
+    /**
+     * Returns the average value of the samples that have been stored in
+     * the monitor or 0.0 if no samples have been added.
+     */
+    double getValueAverage(DataSetType dataSet = FULL);
+
+    /**
+     * Returns the RMS of the sample values that have been stored in
+     * the monitor or 0.0 if no samples have been added.
+     */
+    double getValueRMS(DataSetType dataSet = FULL);
+
+    /**
+     * Returns the minimum sample value that has been stored in
+     * the monitor.
+     */
+    double getValueMin(DataSetType dataSet = FULL);
+
+    /**
+     * Returns the maximum sample value that has been stored in
+     * the monitor.
+     */
+    double getValueMax(DataSetType dataSet = FULL);
+
+    /**
+     * Returns the sample value rate (the sum of all sample values stored
+     * in the monitor divided by the duration) or 0.0 if no samples have been
+     * stored.  The units of the return
+     * value are [the units of the sample values] per second (e.g. MByte/sec).
+     */
+    double getValueRate(DataSetType dataSet = FULL);
+
+    /**
+     * Returns the amount of time (seconds) that this monitor instance has
+     * been processing values starting with the time of the first sample
+     * or 0.0 if no samples have been stored in the monitor.
+     */
+    double getDuration(DataSetType dataSet = FULL);
+
+    /**
+     * Forces a calculation of the statistics for the monitored quantity.
+     * The frequency of the updates to the statistics is driven by how
+     * often this method is called.  It is expected that this method
+     * will be called once per interval specified by
+     * EXPECTED_CALCULATION_INTERVAL.
+     */
+    void calculateStatistics(double currentTime = getCurrentTime());
+
+    /**
+     * Resets the monitor (zeroes out all counters and restarts the
+     * time interval).
+     */
     void reset();
 
+    /**
+     * Enables the monitor (and resets the statistics to provide a
+     * fresh start).
+     */
     void enable();
+
+    /**
+     * Disables the monitor.
+     */
     void disable();
-    void setNewIntervalForRecentData(double interval);
 
+    /**
+     * Specifies a new time interval to be used when calculating
+     * "recent" statistics.
+     */
+    void setNewTimeWindowForRecentResults(double interval);
+
+    /**
+     * Tests whether the monitor is currently enabled.
+     */
     bool isEnabled() const {return enabled_;}
-    double getIntervalForRecentData() const {return intervalForRecentStats_;}
 
+    /**
+     * Returns the length of the time window that has been specified
+     * for recent results.  (This may be different than the actual
+     * length of the recent time window which is affected by the
+     * interval of calls to the calculateStatistics() method.  Use
+     * a getDuration(RECENT) call to determine the actual recent
+     * time window.)
+     */
+    double getTimeWindowForRecentResults() const {
+      return intervalForRecentStats_;
+    }
+
+    /**
+     * Returns the current time as a double.  The value corresponds to the
+     * number of seconds since the epoch (including a fractional part good to
+     * the microsecond level).  A negative value indicates that an error
+     * occurred when fetching the time from the operating system.
+     */
     static double getCurrentTime();
 
   private:
 
-    long long sampleCount_;
-    double startTime_;
-    double overallTotal_;
-    double overallSumOfSquares_;
-    double overallMin_;
-    double overallMax_;
+    double lastCalculationTime_;
+    long long workingSampleCount_;
+    double workingValueSum_;
+    double workingValueSumOfSquares_;
+    double workingValueMin_;
+    double workingValueMax_;
+    boost::mutex accumulationMutex_;
 
-    double lastLatchTime_;
-    long long previousSampleCount_;
-    double previousTotal_;
-    double previousSumOfSquares_;
-    double freshMin_;
-    double freshMax_;
+    int binCount_;
+    int workingBinId_;
+    boost::shared_ptr< std::vector<long long> > binSampleCount_;
+    boost::shared_ptr< std::vector<double> > binValueSum_;
+    boost::shared_ptr< std::vector<double> > binValueSumOfSquares_;
+    boost::shared_ptr< std::vector<double> > binValueMin_;
+    boost::shared_ptr< std::vector<double> > binValueMax_;
+    boost::shared_ptr< std::vector<double> > binDuration_;
+
+    long long fullSampleCount_;
+    double fullSampleRate_;
+    double fullValueSum_;
+    double fullValueSumOfSquares_;
+    double fullValueAverage_;
+    double fullValueRMS_;
+    double fullValueMin_;
+    double fullValueMax_;
+    double fullValueRate_;
+    double fullDuration_;
 
     long long recentSampleCount_;
     double recentSampleRate_;
     double recentValueSum_;
+    double recentValueSumOfSquares_;
     double recentValueAverage_;
     double recentValueRMS_;
     double recentValueMin_;
     double recentValueMax_;
     double recentValueRate_;
     double recentDuration_;
+
+    boost::mutex resultsMutex_;
 
     bool enabled_;
     double intervalForRecentStats_;  // seconds
--- src/MonitoredQuantity.cc
+++ src/MonitoredQuantity.cc
@@ -1,4 +1,4 @@
-// $Id: MonitoredQuantity.cc,v 1.1 2008/04/14 15:42:28 biery Exp $
+// $Id: MonitoredQuantity.cc,v 1.1.2.1 2009/01/29 17:17:15 mommsen Exp $
 
 #include "EventFilter/StorageManager/interface/MonitoredQuantity.h"
 #include <sys/time.h>
@@ -6,320 +6,333 @@
 
 using namespace stor;
 
-/**
- * Default constructor.
- */
-MonitoredQuantity::MonitoredQuantity():
-  enabled_(true),
-  intervalForRecentStats_(30.0)
+MonitoredQuantity::MonitoredQuantity(double timeWindowForRecentResults):
+  enabled_(true)
 {
-  reset();
+  setNewTimeWindowForRecentResults(timeWindowForRecentResults);
 }
 
-/**
- * Adds the specified sample value to the monitor instance.
- */
 void MonitoredQuantity::addSample(double value)
 {
   if (! enabled_) {return;}
 
-  if (sampleCount_ == 0) {
-    startTime_ = getCurrentTime();
-    lastLatchTime_ = startTime_;
+  boost::mutex::scoped_lock sl(accumulationMutex_);
+
+  if (lastCalculationTime_ <= 0.0) {
+    lastCalculationTime_ = getCurrentTime();
   }
 
-  ++sampleCount_;
-  overallTotal_ += value;
-  overallSumOfSquares_ += (value * value);
+  ++workingSampleCount_;
+  workingValueSum_ += value;
+  workingValueSumOfSquares_ += (value * value);
 
-  if (value < overallMin_) overallMin_ = value;
-  if (value > overallMax_) overallMax_ = value;
-
-  if (value < freshMin_) freshMin_ = value;
-  if (value > freshMax_) freshMax_ = value;
+  if (value < workingValueMin_) workingValueMin_ = value;
+  if (value > workingValueMax_) workingValueMax_ = value;
 }
 
-/**
- * Returns the number of samples stored in the monitor.
- */
-long long MonitoredQuantity::getSampleCount(DataSetType dataSet) const
+long long MonitoredQuantity::getSampleCount(DataSetType dataSet)
 {
+  boost::mutex::scoped_lock sl(resultsMutex_);
+
   if (dataSet == RECENT) {
     return recentSampleCount_;
   }
   else {
-    return sampleCount_;
+    return fullSampleCount_;
   }
 }
 
-/**
- * Returns the rate of samples stored in the monitor
- * (number of samples divided by duration).  The units of the
- * return value are samples per second.
- */
-double MonitoredQuantity::getSampleRate(DataSetType dataSet,
-                                   double currentTime) const
+double MonitoredQuantity::getSampleRate(DataSetType dataSet)
 {
+  boost::mutex::scoped_lock sl(resultsMutex_);
+
   if (dataSet == RECENT) {
     return recentSampleRate_;
   }
   else {
-    double duration = getDuration(FULL, currentTime);
-    if (duration > 0.0) {
-      return ((double) sampleCount_) / duration;
-    }
-    else {
-      return 0.0;
-    }
+    return fullSampleRate_;
   }
 }
 
-/**
- * Returns the sum of all sample values stored in the monitor.
- */
-double MonitoredQuantity::getValueSum(DataSetType dataSet) const
+double MonitoredQuantity::getValueSum(DataSetType dataSet)
 {
+  boost::mutex::scoped_lock sl(resultsMutex_);
+
   if (dataSet == RECENT) {
     return recentValueSum_;
   }
   else {
-    return overallTotal_;
+    return fullValueSum_;
   }
 }
 
-/**
- * Returns the average value of the samples that have been stored in
- * the monitor or 0.0 if no samples have been added.
- */
-double MonitoredQuantity::getValueAverage(DataSetType dataSet) const
+double MonitoredQuantity::getValueAverage(DataSetType dataSet)
 {
+  boost::mutex::scoped_lock sl(resultsMutex_);
+
   if (dataSet == RECENT) {
     return recentValueAverage_;
   }
   else {
-    if (sampleCount_ > 0) {
-      return ((double) overallTotal_) / ((double) sampleCount_);
-    }
-    else {
-      return 0.0;
-    }
+    return fullValueAverage_;
   }
 }
 
-/**
- * Returns the RMS of the sample values that have been stored in
- * the monitor or 0.0 if no samples have been added.
- */
-double MonitoredQuantity::getValueRMS(DataSetType dataSet) const
+double MonitoredQuantity::getValueRMS(DataSetType dataSet)
 {
+  boost::mutex::scoped_lock sl(resultsMutex_);
+
   if (dataSet == RECENT) {
     return recentValueRMS_;
   }
   else {
-    if (sampleCount_ > 0) {
-      double squareAvg = (overallSumOfSquares_ / (double) sampleCount_);
-      double avg = (overallTotal_ / (double) sampleCount_);
-      double sigSquared = squareAvg - avg*avg;
-      if(sigSquared > 0.0) {
-        return sqrt(sigSquared);
-      }
-      else {
-        return 0.0;
-      }
-    }
-    else {
-      return 0.0;
-    }
+    return fullValueRMS_;
   }
 }
 
-/**
- * Returns the minimum sample value that has been stored in
- * the monitor.
- */
-double MonitoredQuantity::getValueMin(DataSetType dataSet) const
+double MonitoredQuantity::getValueMin(DataSetType dataSet)
 {
+  boost::mutex::scoped_lock sl(resultsMutex_);
+
   if (dataSet == RECENT) {
     return recentValueMin_;
   }
   else {
-    return overallMin_;
+    return fullValueMin_;
   }
 }
 
-/**
- * Returns the maximum sample value that has been stored in
- * the monitor.
- */
-double MonitoredQuantity::getValueMax(DataSetType dataSet) const
+double MonitoredQuantity::getValueMax(DataSetType dataSet)
 {
+  boost::mutex::scoped_lock sl(resultsMutex_);
+
   if (dataSet == RECENT) {
     return recentValueMax_;
   }
   else {
-    return overallMax_;
+    return fullValueMax_;
   }
 }
 
-/**
- * Returns the sample value rate (the sum of all sample values stored
- * in the monitor divided by the duration) or 0.0 if no samples have been
- * stored.  The units of the return
- * value are [the units of the sample values] per second (e.g. MByte/sec).
- */
-double MonitoredQuantity::getValueRate(DataSetType dataSet,
-                                  double currentTime) const
+double MonitoredQuantity::getValueRate(DataSetType dataSet)
 {
+  boost::mutex::scoped_lock sl(resultsMutex_);
+
   if (dataSet == RECENT) {
-    return recentValueAverage_;
+    return recentValueRate_;
   }
   else {
-    double duration = getDuration(FULL, currentTime);
-    if (duration > 0.0) {
-      return ((double) overallTotal_) / duration;
-    }
-    else {
-      return 0.0;
-    }
+    return fullValueRate_;
   }
 }
 
-/**
- * Returns the amount of time (seconds) that this monitor instance has
- * been processing values starting with the time of the first sample
- * or 0.0 if no samples have been stored in the monitor.
- */
-double MonitoredQuantity::getDuration(DataSetType dataSet,
-                                 double currentTime) const
+double MonitoredQuantity::getDuration(DataSetType dataSet)
 {
+  boost::mutex::scoped_lock sl(resultsMutex_);
+
   if (dataSet == RECENT) {
     return recentDuration_;
   }
   else {
-    if (sampleCount_ == 0) {
-      return 0.0;
-    }
-    else {
-      return (currentTime - startTime_);
-    }
+    return fullDuration_;
   }
 }
 
-/**
- * Updates the statistics for the "recent" data set.  This method
- * currently ends one "recent" interval and starts another.
- */
-void MonitoredQuantity::latchRecentValuesIfNeeded(double currentTime)
+void MonitoredQuantity::calculateStatistics(double currentTime)
 {
   if (! enabled_) {return;}
-  if (sampleCount_ == 0) {return;}
-  if ((currentTime - lastLatchTime_) < intervalForRecentStats_) {return;}
+  if (lastCalculationTime_ <= 0.0) {return;}
 
-  recentSampleCount_ = sampleCount_ - previousSampleCount_;
-  previousSampleCount_ = sampleCount_;
+  // create local copies of the working values to minimize the
+  // time that we could block a thread trying to add a sample.
+  // Also, reset the working values.
+  long long latestSampleCount;
+  double latestValueSum;
+  double latestValueSumOfSquares;
+  double latestValueMin;
+  double latestValueMax;
+  double latestDuration;
+  {
+    boost::mutex::scoped_lock sl(accumulationMutex_);
 
-  recentDuration_ = currentTime - lastLatchTime_;
-  lastLatchTime_ = currentTime;
+    latestSampleCount = workingSampleCount_;
+    latestValueSum = workingValueSum_;
+    latestValueSumOfSquares = workingValueSumOfSquares_;
+    latestValueMin = workingValueMin_;
+    latestValueMax = workingValueMax_;
+    latestDuration = currentTime - lastCalculationTime_;
 
-  if (recentDuration_ > 0.0) {
-    recentSampleRate_ = ((double) recentSampleCount_) / recentDuration_;
+    lastCalculationTime_ = currentTime;
+    workingSampleCount_ = 0;
+    workingValueSum_ = 0.0;
+    workingValueSumOfSquares_ = 0.0;
+    workingValueMin_ =  999999999.0;
+    workingValueMax_ = -999999999.0;
   }
-  else {
-    recentSampleRate_ = 0.0;
-  }
 
-  recentValueSum_ = overallTotal_ - previousTotal_;
-  previousTotal_ = overallTotal_;
+  // lock out any interaction with the results while we update them
+  {
+    boost::mutex::scoped_lock sl(resultsMutex_);
 
-  if (recentSampleCount_ > 0) {
-    recentValueAverage_ = recentValueSum_ / ((double) recentSampleCount_);
-  }
-  else {
-    recentValueAverage_ = 0.0;
-  }
+    // we simply add the latest results to the full set
+    fullSampleCount_ += latestSampleCount;
+    fullValueSum_ += latestValueSum;
+    fullValueSumOfSquares_ += latestValueSumOfSquares;
+    if (latestValueMin < fullValueMin_) {fullValueMin_ = latestValueMin;}
+    if (latestValueMax < fullValueMax_) {fullValueMax_ = latestValueMax;}
+    fullDuration_ += latestDuration;
 
-  if (recentSampleCount_ > 0) {
-    double squareAvg = ((overallSumOfSquares_ - previousSumOfSquares_)
-                         / (double) recentSampleCount_);
-    previousSumOfSquares_ = overallSumOfSquares_;
-    double avg = (recentValueSum_ / (double) recentSampleCount_);
-    double sigSquared = squareAvg - avg*avg;
-    if(sigSquared > 0.0) {
-      recentValueRMS_ = sqrt(sigSquared);
+    // for the recent results, we need to replace the contents of
+    // the working bin and re-calculate the recent values
+    (*binSampleCount_)[workingBinId_] = latestSampleCount;
+    (*binValueSum_)[workingBinId_] = latestValueSum;
+    (*binValueSumOfSquares_)[workingBinId_] = latestValueSumOfSquares;
+    (*binValueMin_)[workingBinId_] = latestValueMin;
+    (*binValueMax_)[workingBinId_] = latestValueMax;
+    (*binDuration_)[workingBinId_] = latestDuration;
+
+    recentSampleCount_ = 0;
+    recentValueSum_ = 0.0;
+    recentValueSumOfSquares_ = 0.0;
+    recentValueMin_ =  999999999.0;
+    recentValueMax_ = -999999999.0;
+    recentDuration_ = 0.0;
+
+    for (int idx = 0; idx < binCount_; ++idx) {
+      recentSampleCount_ += (*binSampleCount_)[idx];
+      recentValueSum_ += (*binValueSum_)[idx];
+      recentValueSumOfSquares_ += (*binValueSumOfSquares_)[idx];
+      if ((*binValueMin_)[idx] < recentValueMin_) {
+        recentValueMin_ = (*binValueMin_)[idx];
+      }
+      if ((*binValueMax_)[idx] < recentValueMax_) {
+        recentValueMax_ = (*binValueMax_)[idx];
+      }
+      recentDuration_ = (*binDuration_)[idx];
+    }
+
+    // update the working bin ID here so that we are ready for
+    // the next calculation request
+    ++workingBinId_;
+    if (workingBinId_ >= binCount_) {workingBinId_ = 0;}
+
+    // calculate the derived full values
+    if (fullDuration_ > 0.0) {
+      fullSampleRate_ = ((double) fullSampleCount_) / fullDuration_;
+      fullValueRate_ = ((double) fullValueSum_) / fullDuration_;
     }
     else {
+      fullSampleRate_ = 0.0;
+      fullValueRate_ = 0.0;
+    }
+
+    if (fullSampleCount_ > 0) {
+      fullValueAverage_ = fullValueSum_ / ((double) fullSampleCount_);
+
+      double squareAvg = (fullValueSumOfSquares_ / (double) fullSampleCount_);
+      double avg = (fullValueSum_ / (double) fullSampleCount_);
+      double sigSquared = squareAvg - avg*avg;
+      if(sigSquared > 0.0) {
+        fullValueRMS_ = sqrt(sigSquared);
+      }
+      else {
+        fullValueRMS_ = 0.0;
+      }
+    }
+    else {
+      fullValueAverage_ = 0.0;
+      fullValueRMS_ = 0.0;
+    }
+
+    // calculate the derived recent values
+    if (recentDuration_ > 0.0) {
+      recentSampleRate_ = ((double) recentSampleCount_) / recentDuration_;
+      recentValueRate_ = ((double) recentValueSum_) / recentDuration_;
+    }
+    else {
+      recentSampleRate_ = 0.0;
+      recentValueRate_ = 0.0;
+    }
+
+    if (recentSampleCount_ > 0) {
+      recentValueAverage_ = recentValueSum_ / ((double) recentSampleCount_);
+
+      double squareAvg = (recentValueSumOfSquares_ /
+                          (double) recentSampleCount_);
+      double avg = (recentValueSum_ / (double) recentSampleCount_);
+      double sigSquared = squareAvg - avg*avg;
+      if(sigSquared > 0.0) {
+        recentValueRMS_ = sqrt(sigSquared);
+      }
+      else {
+        recentValueRMS_ = 0.0;
+      }
+    }
+    else {
+      recentValueAverage_ = 0.0;
       recentValueRMS_ = 0.0;
     }
   }
-  else {
-    recentValueRMS_ = 0.0;
-  }
-
-  if (recentSampleCount_ > 0) {
-    recentValueMin_ = freshMin_;
-    recentValueMax_ = freshMax_;
-  }
-  else {
-    recentValueMin_ = 0.0;
-    recentValueMax_ = 0.0;
-  }
-  freshMin_ =  999999999.0;
-  freshMax_ = -999999999.0;
-
-  if (recentDuration_ > 0.0) {
-    recentValueRate_ = ((double) recentValueSum_) / recentDuration_;
-  }
-  else {
-    recentValueRate_ = 0.0;
-  }
 }
 
-/**
- * Resets the monitor (zeroes out all counters and restarts the
- * time interval).
- */
 void MonitoredQuantity::reset()
 {
-    sampleCount_ = 0;
-    startTime_ = 0.0;
-    overallTotal_ = 0.0;
-    overallSumOfSquares_ = 0.0;
-    overallMin_ =  999999999.0;
-    overallMax_ = -999999999.0;
+  {
+    boost::mutex::scoped_lock sl(accumulationMutex_);
 
-    lastLatchTime_ = 0.0;
-    previousSampleCount_ = 0;;
-    previousTotal_ = 0.0;
-    previousSumOfSquares_ = 0.0;
-    freshMin_ =  999999999.0;
-    freshMax_ = -999999999.0;
+    lastCalculationTime_ = -1.0;
+    workingSampleCount_ = 0;
+    workingValueSum_ = 0.0;
+    workingValueSumOfSquares_ = 0.0;
+    workingValueMin_ =  999999999.0;
+    workingValueMax_ = -999999999.0;
+  }
+
+  {
+    boost::mutex::scoped_lock sl(resultsMutex_);
+
+    workingBinId_ = 0;
+    for (int idx = 0; idx < binCount_; ++idx) {
+      (*binSampleCount_)[idx] = 0;
+      (*binValueSum_)[idx] = 0.0;
+      (*binValueSumOfSquares_)[idx] = 0.0;
+      (*binValueMin_)[idx] =  999999999.0;
+      (*binValueMax_)[idx] = -999999999.0;
+      (*binDuration_)[idx] = 0.0;
+    }
+
+    fullSampleCount_ = 0;
+    fullSampleRate_ = 0.0;
+    fullValueSum_ = 0.0;
+    fullValueSumOfSquares_ = 0.0;
+    fullValueAverage_ = 0.0;
+    fullValueRMS_ = 0.0;
+    fullValueMin_ =  999999999.0;
+    fullValueMax_ = -999999999.0;
+    fullValueRate_ = 0.0;
+    fullDuration_ = 0.0;
 
     recentSampleCount_ = 0;
     recentSampleRate_ = 0.0;
     recentValueSum_ = 0.0;
+    recentValueSumOfSquares_ = 0.0;
     recentValueAverage_ = 0.0;
     recentValueRMS_ = 0.0;
-    recentValueMin_ = 0.0;
-    recentValueMax_ = 0.0;
+    recentValueMin_ =  999999999.0;
+    recentValueMax_ = -999999999.0;
     recentValueRate_ = 0.0;
     recentDuration_ = 0.0;
+  }
 }
 
-/**
- * Enables the monitor (and resets the statistics to provide a
- * fresh start).
- */
 void MonitoredQuantity::enable()
 {
   if (! enabled_) {
-    reset();  // do this first to provide some internal consistency
+    reset();
     enabled_ = true;
   }
 }
 
-/**
- * Disables the monitor.
- */
 void MonitoredQuantity::disable()
 {
   if (enabled_) {
@@ -327,21 +340,49 @@ void MonitoredQuantity::disable()
   }
 }
 
-/**
- * Specifies a new time interval to be used when calculating
- * "recent" statistics.
- */
-void MonitoredQuantity::setNewIntervalForRecentData(double interval)
+void MonitoredQuantity::setNewTimeWindowForRecentResults(double interval)
 {
-  intervalForRecentStats_ = interval;
+  // lock the results objects since we're dramatically changing the
+  // bins used for the recent results
+  {
+    boost::mutex::scoped_lock sl(resultsMutex_);
+
+    intervalForRecentStats_ = interval;
+
+    // determine how many bins we should use in our sliding window
+    // by dividing the input time window by the expected calculation
+    // interval and rounding to the nearest integer.
+    binCount_ =
+      static_cast<int>((intervalForRecentStats_ / 
+                        static_cast<double>(EXPECTED_CALCULATION_INTERVAL)) +
+                       0.5);
+
+    // create the vectors for the binned quantities
+    binSampleCount_.reset(new std::vector<long long>());
+    binValueSum_.reset(new std::vector<double>());
+    binValueSumOfSquares_.reset(new std::vector<double>());
+    binValueMin_.reset(new std::vector<double>());
+    binValueMax_.reset(new std::vector<double>());
+    binDuration_.reset(new std::vector<double>());
+
+    // populate the vectors with initial values (since everything
+    // after this point assumes that the vectors have the correct
+    // number of entries)
+    for (int idx = 0; idx < binCount_; ++idx) {
+      this->binSampleCount_->push_back(0);
+      this->binValueSum_->push_back(0.0);
+      this->binValueSumOfSquares_->push_back(0.0);
+      this->binValueMin_->push_back(0.0);
+      this->binValueMax_->push_back(0.0);
+      this->binDuration_->push_back(0.0);
+    }
+  }
+
+  // call the reset method to populate the correct initial values
+  // for the internal sample data
+  reset();
 }
 
-/**
- * Returns the current time as a double.  The value corresponds to the
- * number of seconds since the epoch (including a fractional part good to
- * the microsecond level).  A negative value indicates that an error
- * occurred when fetching the time from the operating system.
- */
 double MonitoredQuantity::getCurrentTime()
 {
   double now = -1.0;
