18fe3dfb21029a107cc841ddb75584801bb72f05
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2009-02-04 17:51:25 +0000

    An initial version of exceptions inside the stor namespace. This will need to be extended as we move along towards the sentinel error reporting.

---
+++ interface/Exception.h
@@ -0,0 +1,59 @@
+// $Id: MonitorCollection.cc,v 1.1.2.1 2009/02/04 13:27:08 mommsen Exp $
+
+#ifndef _storagemanager_Exception_h_
+#define _storagemanager_Exception_h_
+
+
+#include "xcept/Exception.h"
+
+// The following macro is defined in newer xdaq versions
+#ifndef XCEPT_DEFINE_EXCEPTION
+#define XCEPT_DEFINE_EXCEPTION(NAMESPACE1, EXCEPTION_NAME) \
+namespace NAMESPACE1 { \
+namespace exception { \
+class EXCEPTION_NAME: public xcept::Exception \
+{\
+        public: \
+        EXCEPTION_NAME( std::string name, std::string message, std::string module, int line, std::string function ): \
+                xcept::Exception(name, message, module, line, function) \
+        {} \
+        EXCEPTION_NAME( std::string name, std::string message, std::string module, int line, std::string function, xcept::Exception & e ): \
+                xcept::Exception(name, message, module, line, function,e) \
+        {} \
+}; \
+} \
+}
+#endif
+
+
+
+/**
+ * Generic exception raised by the storage manager
+ */
+XCEPT_DEFINE_EXCEPTION(stor, Exception)
+
+/**
+ * Exception raised in case of a SOAP error
+ */
+XCEPT_DEFINE_EXCEPTION(stor, SoapMessage)
+
+/**
+ * Exception raised in case of a finite state machine error
+ */
+XCEPT_DEFINE_EXCEPTION(stor, StateMachine)
+
+/**
+ * Exception raised in case of a monitoring error
+ */
+XCEPT_DEFINE_EXCEPTION(stor, Monitoring)
+
+
+#endif // _storagemanager_Exception_h_
+
+
+/// emacs configuration
+/// Local Variables: -
+/// mode: c++ -
+/// c-basic-offset: 2 -
+/// indent-tabs-mode: nil -
+/// End: -
