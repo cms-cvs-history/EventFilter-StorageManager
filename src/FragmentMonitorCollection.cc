69a5eaa70c313ad483818b699db349cc0d1f382c
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2009-02-05 14:51:46 +0000

    Start of Received Data Statistics HTML snipped.

--- src/FragmentMonitorCollection.cc
+++ src/FragmentMonitorCollection.cc
@@ -1,7 +1,11 @@
-// $Id: FragmentMonitorCollection.cc,v 1.1.2.4 2009/02/05 09:51:24 mommsen Exp $
+// $Id: FragmentMonitorCollection.cc,v 1.1.2.5 2009/02/05 10:09:49 mommsen Exp $
+
+#include <sstream>
+#include <iomanip>
 
 #include "EventFilter/StorageManager/interface/Exception.h"
 #include "EventFilter/StorageManager/interface/FragmentMonitorCollection.h"
+#include "EventFilter/StorageManager/interface/XHTMLMaker.h"
 
 using namespace stor;
 
@@ -55,10 +59,77 @@ void FragmentMonitorCollection::do_updateInfoSpace()
 }
 
 
-xercesc::DOMElement* FragmentMonitorCollection::do_addDOMElement(xercesc::DOMElement *parent)
+void FragmentMonitorCollection::do_addDOMElement(xercesc::DOMElement *parent)
 {
-  //Create the HTML snipped from ownded MonitoredQuantities
-  return parent;
+  XHTMLMaker* maker = XHTMLMaker::instance();
+
+  XHTMLMaker::AttrMap tableAttr;
+  tableAttr[ "frame" ] = "void";
+  tableAttr[ "rules" ] = "group";
+  tableAttr[ "class" ] = "states";
+
+  XHTMLMaker::AttrMap tableHeaderAttr;
+  tableHeaderAttr[ "colspan" ] = "2";
+
+  XHTMLMaker::AttrMap tableValueAttr;
+  tableValueAttr[ "align" ] = "right";
+
+  XHTMLMaker::Node* table = maker->addNode("table", parent, tableAttr);
+
+  // Received Data Statistics header
+  XHTMLMaker::Node* tableRow = maker->addNode("tr", table);
+  XHTMLMaker::Node* tableHeader = maker->addNode("th", tableRow, tableHeaderAttr);
+  maker->addText(tableHeader, "Received Data Statistics");
+
+  // Parameter/Value header
+  tableRow = maker->addNode("tr", table);
+  tableHeader = maker->addNode("th", tableRow);
+  maker->addText(tableHeader, "Parameter");
+  tableHeader = maker->addNode("th", tableRow);
+  maker->addText(tableHeader, "Value");
+
+  // Frames received entry
+  tableRow = maker->addNode("tr", table);
+  XHTMLMaker::Node* tableDiv = maker->addNode("td", tableRow);
+  maker->addText(tableDiv, "Frames Received");
+  tableDiv = maker->addNode("td", tableRow, tableValueAttr);
+  std::stringstream framesString;
+  framesString << eventFragmentSizes.getSampleCount();
+  maker->addText(tableDiv, framesString.str());
+
+  // DQM records received entry
+  tableRow = maker->addNode("tr", table);
+  tableDiv = maker->addNode("td", tableRow);
+  maker->addText(tableDiv, "DQM Records Received");
+  tableDiv = maker->addNode("td", tableRow, tableValueAttr);
+  std::stringstream recordsString;
+  recordsString << "n/a";
+  maker->addText(tableDiv, recordsString.str());
+
+  // Memory usage does not belong here
+
+  // Recent statistics header
+  tableRow = maker->addNode("tr", table);
+  tableHeader = maker->addNode("th", tableRow, tableHeaderAttr);
+  std::stringstream durationString;
+  durationString << "Statistics for last " <<
+    std::fixed << std::setprecision(1) <<
+    eventFragmentSizes.getDuration(MonitoredQuantity::RECENT) << " sec";
+  maker->addText(tableHeader, durationString.str());
+
+  // DQM records received entry
+  tableRow = maker->addNode("tr", table);
+  tableDiv = maker->addNode("td", tableRow);
+  maker->addText(tableDiv, "Bandwidth (MB/s)");
+  tableDiv = maker->addNode("td", tableRow, tableValueAttr);
+  std::stringstream bandwidthString;
+  bandwidthString << 
+    std::fixed << std::setprecision(2) <<
+    eventFragmentSizes.getValueRate(MonitoredQuantity::RECENT);
+  maker->addText(tableDiv, bandwidthString.str());
+
+  // etc...
+
 }
 
 
