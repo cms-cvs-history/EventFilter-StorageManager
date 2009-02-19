6df3adcddf8a54655aeec856e0962486b19f3dbd
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2009-02-19 10:24:00 +0000

    Tests for stale events and small improvements to other tests.

--- test/FragmentStore_t.cpp
+++ test/FragmentStore_t.cpp
@@ -26,6 +26,9 @@ class testFragmentStore : public CppUnit::TestFixture
   CPPUNIT_TEST(testSingleFragment);
   CPPUNIT_TEST(testMultipleFragments);
   CPPUNIT_TEST(testConcurrentFragments);
+  CPPUNIT_TEST(testStaleEvent);
+  CPPUNIT_TEST(testSingleIncompleteEvent);
+  CPPUNIT_TEST(testOneOfManyIncompleteEvent);
 
   CPPUNIT_TEST_SUITE_END();
 
@@ -33,8 +36,16 @@ public:
   void testSingleFragment();
   void testMultipleFragments();
   void testConcurrentFragments();
+  void testStaleEvent();
+  void testSingleIncompleteEvent();
+  void testOneOfManyIncompleteEvent();
 
 private:
+  unsigned int getEventNumber
+  (
+    const unsigned int eventCount
+  );
+
   stor::I2OChain getFragment
   (
     const unsigned int eventCount,
@@ -46,6 +57,16 @@ private:
 
 };
 
+unsigned int testFragmentStore::getEventNumber
+(
+  const unsigned int eventCount
+)
+{
+  unsigned int eventNumber = 0xb4b4e1e1;
+  eventNumber <<= eventCount;
+  return eventNumber;
+}
+
 stor::I2OChain testFragmentStore::getFragment
 (
   const unsigned int eventCount,
@@ -53,16 +74,13 @@ stor::I2OChain testFragmentStore::getFragment
   const unsigned int totalFragments
 )
 {
-  unsigned int eventNumber = 0xb4b4e1e1;
-  eventNumber <<= eventCount;
-
   Reference* ref = 
     allocate_frame_with_basic_header(I2O_SM_DATA, fragmentCount, totalFragments);
   I2O_SM_DATA_MESSAGE_FRAME *i2oMsg =
     (I2O_SM_DATA_MESSAGE_FRAME*) ref->getDataLocation();
   i2oMsg->rbBufferID = 2;
   i2oMsg->runID = 0xa5a5d2d2;
-  i2oMsg->eventID = eventNumber;
+  i2oMsg->eventID = getEventNumber(eventCount);
   i2oMsg->outModID = 0xc3c3f0f0;
   i2oMsg->fuProcID = 0x01234567;
   i2oMsg->fuGUID = 0x89abcdef;
@@ -99,11 +117,12 @@ void testFragmentStore::testMultipleFragments()
     if ( i < totalFragments-1 )
     {
       CPPUNIT_ASSERT(!complete);
-      CPPUNIT_ASSERT(!frag.complete());
+      CPPUNIT_ASSERT(frag.empty());
     }
     else
     {
       CPPUNIT_ASSERT(complete);
+      CPPUNIT_ASSERT(!frag.empty());
       CPPUNIT_ASSERT(frag.complete());
       CPPUNIT_ASSERT(!frag.faulty());
       CPPUNIT_ASSERT(frag.fragmentCount() == totalFragments);
@@ -117,8 +136,8 @@ void testFragmentStore::testConcurrentFragments()
 {
   CPPUNIT_ASSERT(outstanding_bytes() == 0);
 
-  const unsigned int totalFragments = 3;
-  const unsigned int totalEvents = 5;
+  const unsigned int totalFragments = 5;
+  const unsigned int totalEvents = 15;
 
   for (unsigned int fragNum = 0; fragNum < totalFragments; ++fragNum)
   {
@@ -130,7 +149,7 @@ void testFragmentStore::testConcurrentFragments()
       if ( fragNum < totalFragments-1 )
       {
         CPPUNIT_ASSERT(!complete);
-        CPPUNIT_ASSERT(!frag.complete());
+        CPPUNIT_ASSERT(frag.empty());
       }
       else
       {
@@ -138,13 +157,117 @@ void testFragmentStore::testConcurrentFragments()
         CPPUNIT_ASSERT(frag.complete());
         CPPUNIT_ASSERT(!frag.faulty());
         CPPUNIT_ASSERT(frag.fragmentCount() == totalFragments);
+
+        stor::FragKey fragmentKey = frag.fragmentKey();
+        CPPUNIT_ASSERT(fragmentKey.event_ == getEventNumber(eventNum));
       }
     }
   }
   CPPUNIT_ASSERT(outstanding_bytes() == 0);
 }
 
+void testFragmentStore::testStaleEvent()
+{
+  CPPUNIT_ASSERT(outstanding_bytes() == 0);
+  {
+    stor::I2OChain frag = getFragment(1,0,1);
+    CPPUNIT_ASSERT(frag.complete());
+    bool hasStaleEvent = fragmentStore.getStaleEvent(frag, 0);
+    CPPUNIT_ASSERT(hasStaleEvent == false);
+    CPPUNIT_ASSERT(frag.empty());
+  }
+  CPPUNIT_ASSERT(outstanding_bytes() == 0);
+}
 
+void testFragmentStore::testSingleIncompleteEvent()
+{
+  CPPUNIT_ASSERT(outstanding_bytes() == 0);
+  {
+    const unsigned int totalFragments = 5;
+    stor::I2OChain frag;
+    bool complete;
+    
+    for (unsigned int i = 0; i < totalFragments-1; ++i)
+    {
+      frag = getFragment(1,i,totalFragments);
+      CPPUNIT_ASSERT(!frag.complete());
+      complete = fragmentStore.addFragment(frag);
+    }
+    CPPUNIT_ASSERT(frag.empty());
+    
+    CPPUNIT_ASSERT(fragmentStore.getStaleEvent(frag, 0) == true);
+    CPPUNIT_ASSERT(!frag.complete());
+    CPPUNIT_ASSERT(frag.faulty());
+    CPPUNIT_ASSERT(frag.fragmentCount() == totalFragments-1);
+    
+    stor::FragKey fragmentKey = frag.fragmentKey();
+    CPPUNIT_ASSERT(fragmentKey.event_ == getEventNumber(1));
+  }
+  CPPUNIT_ASSERT(outstanding_bytes() == 0);
+}
+
+
+void testFragmentStore::testOneOfManyIncompleteEvent()
+{
+  CPPUNIT_ASSERT(outstanding_bytes() == 0);
+  {
+    const unsigned int totalFragments = 5;
+    const unsigned int totalEvents = 15;
+    stor::I2OChain frag;
+    bool complete;
+    
+    // Fill one event with a missing fragment
+    for (unsigned int fragNum = 0; fragNum < totalFragments-1; ++fragNum)
+    {
+      frag = getFragment(0,fragNum,totalFragments);
+      CPPUNIT_ASSERT(!frag.complete());
+      complete = fragmentStore.addFragment(frag);
+    }
+    CPPUNIT_ASSERT(!complete);
+    CPPUNIT_ASSERT(frag.empty());
+    
+    // Sleep for a second
+    sleep(1);
+    
+    // Fill more events with missing fragmentes
+    for (unsigned int eventNum = 1; eventNum < totalEvents; ++eventNum)
+    {
+      for (unsigned int fragNum = 1; fragNum < totalFragments; ++fragNum)
+      {
+        frag = getFragment(eventNum,fragNum,totalFragments);
+        CPPUNIT_ASSERT(!frag.complete());
+        complete = fragmentStore.addFragment(frag);
+      }
+      CPPUNIT_ASSERT(!complete);
+      CPPUNIT_ASSERT(frag.empty());
+    }
+
+    // Get the first event which should be stale by now
+    CPPUNIT_ASSERT(fragmentStore.getStaleEvent(frag, 1) == true);
+    CPPUNIT_ASSERT(!frag.complete());
+    CPPUNIT_ASSERT(frag.faulty());
+    CPPUNIT_ASSERT(frag.fragmentCount() == totalFragments-1);
+    
+    stor::FragKey fragmentKey = frag.fragmentKey();
+    CPPUNIT_ASSERT(fragmentKey.event_ == getEventNumber(0));
+    
+    // Finish the other events
+    for (unsigned int eventNum = 1; eventNum < totalEvents; ++eventNum)
+    {
+      frag = getFragment(eventNum,0,totalFragments);
+      CPPUNIT_ASSERT(!frag.complete());
+      complete = fragmentStore.addFragment(frag);
+      CPPUNIT_ASSERT(complete);
+      CPPUNIT_ASSERT(frag.complete());
+      CPPUNIT_ASSERT(!frag.faulty());
+      CPPUNIT_ASSERT(frag.fragmentCount() == totalFragments);
+    
+      stor::FragKey fragmentKey = frag.fragmentKey();
+      CPPUNIT_ASSERT(fragmentKey.event_ == getEventNumber(eventNum));
+    }
+  }
+  CPPUNIT_ASSERT(outstanding_bytes() == 0);
+}
 
 
 // This macro writes the 'main' for this test.
