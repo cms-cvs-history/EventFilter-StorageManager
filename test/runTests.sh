e779a5382eb01df133f936adb9744bc597eb523a
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2009-03-10 15:29:36 +0000

    Add all new tests.

--- test/runTests.sh
+++ test/runTests.sh
@@ -4,14 +4,32 @@
 
 source testSetup.sh
 
-
-tests="fsmSoap/fsmSoap.tcl"
+tests="\
+ CommandQueue_t\
+ ConcurrentQueue_t\
+ DQMEventConsumerRegistrationInfo_t\
+ EventConsumerRegistrationInfo_t\
+ EventDistributor_t\
+ EventStreamConfigurationInfo_t\
+ FragmentStore_t\
+ I2OChain_t\
+ MemoryChainDuplicate_t\
+ MonitoredQuantity_t\
+ processCount_t\
+ QueueID_t\
+ RunCollector_t\
+ RunnerTest_t\
+ state_machine_t\
+ SyncQueue_t\
+ test_condition\
+ workloop_t\
+ xhtmlmaker_t"
 
 error=0
 
 for test in $tests ; do
     startTime=`date "+%H:%M:%S"`
-    printf "%-32s: $startTime " $test
+    printf "%-36s: $startTime " $test
     
     if $test > $test.log 2>&1
 	then
