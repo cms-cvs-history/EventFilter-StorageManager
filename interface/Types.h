d40ce5b1fcace2b86aefc5f82d51aa1bf1e6c50b
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2009-03-10 15:32:42 +0000

    No more QueueID typedef. This file is now an empty shell, but could be reused later.

--- interface/Types.h
+++ interface/Types.h
@@ -1,4 +1,4 @@
-// $Id: Types.h,v 1.1.2.1 2009/01/30 10:49:40 mommsen Exp $
+// $Id: Types.h,v 1.1.2.2 2009/01/30 20:34:34 paterno Exp $
 
 #ifndef StorageManager_Types_h
 #define StorageManager_Types_h
@@ -10,12 +10,10 @@ namespace stor {
   /**
    * Definition of types used in the storage manager
    *
-   * $Author: mommsen $
-   * $Revision: 1.1.2.1 $
-   * $Date: 2009/01/30 10:49:40 $
+   * $Author: paterno $
+   * $Revision: 1.1.2.2 $
+   * $Date: 2009/01/30 20:34:34 $
    */
-
-  typedef size_t QueueID;
 
   
 } // namespace stor
