bb46f18a1b8e9dd0126147c5734ce992890e92d2
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2009-03-13 15:10:37 +0000

    Better put the right revision back. Sorry for the mess.

--- interface/ProgressMarker.h
+++ interface/ProgressMarker.h
@@ -1,6 +1,26 @@
 #if !defined(STOR_PROGRESSMARKER_H)
 #define STOR_PROGRESSMARKER_H
 
+// Created by Markus Klute on 2007 Jan 09.
+// $Id: ProgressMarker.h,v 1.1 2007/02/05 11:19:56 klute Exp $
+
+// singleton class 
+// to return the status of the StorageManager
+// with std::string status()
+
+// used for monitoring only, the status is implemented 
+// in the following way
+//
+// thread/status
+// ----------------------------
+// read     oox  xx   o    o
+// process  xxx  oo   o    o
+// write    oxx  ox   x    o
+// ----------------------------
+// result   proc inpu outp idle
+
+#include <string>
+
 namespace stor 
 {
   class ProgressMarker
