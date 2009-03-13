67d15c875cfb16dd62112ac54e33b8462567e860
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2009-03-13 10:24:32 +0000

    Add a test for poping an event from a non-existing queue.

--- test/EventQueueCollection_t.cpp
+++ test/EventQueueCollection_t.cpp
@@ -2,10 +2,13 @@
 #include "cppunit/extensions/HelperMacros.h"
 
 #include "EventFilter/StorageManager/interface/EventQueueCollection.h"
+#include "EventFilter/StorageManager/interface/Exception.h"
+#include "EventFilter/StorageManager/interface/I2OChain.h"
 #include "EventFilter/StorageManager/interface/QueueID.h"
 #include "EventFilter/StorageManager/interface/EnquingPolicyTag.h"
 
 using stor::EventQueueCollection;
+using stor::I2OChain;
 using stor::QueueID;
 
 class testEventQueueCollection : public CppUnit::TestFixture
@@ -13,7 +16,7 @@ class testEventQueueCollection : public CppUnit::TestFixture
   CPPUNIT_TEST_SUITE(testEventQueueCollection);
 
   CPPUNIT_TEST(create_queues);
-
+  CPPUNIT_TEST(pop_event_from_non_existing_queue);
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -21,6 +24,7 @@ public:
   void tearDown();
 
   void create_queues();
+  void pop_event_from_non_existing_queue();
 
 private:
   // No data members yet.
@@ -59,6 +63,41 @@ testEventQueueCollection::create_queues()
   QueueID id4 = c.createQueue(stor::enquing_policy::FailIfFull, 1);
   CPPUNIT_ASSERT(!id4.isValid());
   CPPUNIT_ASSERT(c.size() == 3);
+}
+
+void 
+testEventQueueCollection::pop_event_from_non_existing_queue()
+{
+  // Default collection should have no queues.
+  EventQueueCollection c;
+  CPPUNIT_ASSERT(c.size() == 0);
+
+  // Add one queue
+  QueueID id1 = c.createQueue(stor::enquing_policy::DiscardNew, 10);
+  CPPUNIT_ASSERT(id1.policy() == stor::enquing_policy::DiscardNew);
+  CPPUNIT_ASSERT(c.size() == 1);
+
+  // Add another queue
+  QueueID id2 = c.createQueue(stor::enquing_policy::DiscardOld, 20);
+  CPPUNIT_ASSERT(id2.policy() == stor::enquing_policy::DiscardOld);
+  CPPUNIT_ASSERT(c.size() == 2);
+
+  // A non-existent queue
+  QueueID id3 = c.createQueue(stor::enquing_policy::FailIfFull, 1);
+  CPPUNIT_ASSERT(!id3.isValid());
+  CPPUNIT_ASSERT(c.size() == 2);
+
+  I2OChain chain;
+
+  try
+  {
+    chain = c.popEvent(id3);
+    CPPUNIT_ASSERT(false);
+  }
+  catch (stor::exception::UnknownQueueId)
+  {
+  }
+  CPPUNIT_ASSERT(chain.empty());
 }
 
 // This macro writes the 'main' for this test.
