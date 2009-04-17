161c6c81642bad30f0a2361eda0a6dc63ca24bbf
Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch>
2009-04-17 17:27:14 +0000

    Key for DQM events.

---
+++ interface/DQMKey.h
@@ -0,0 +1,52 @@
+// $Id: DQMKey.h,v 1.1.2.1 2009/03/01 20:36:29 biery Exp $
+
+#ifndef StorageManager_DQMKey_h
+#define StorageManager_DQMKey_h
+
+#include <cstddef>
+
+namespace stor {
+
+  /**
+   * Definition of the DQMKey used in the storage manager
+   *
+   * $Author: biery $
+   * $Revision: 1.1.2.1 $
+   * $Date: 2009/03/01 20:36:29 $
+   */
+  
+  struct DQMKey
+  {
+    uint32 runNumber;
+    uint32 lumiSection;
+    uint32 updateNumber;
+
+    bool operator<(DQMKey const& other) const;
+    bool operator==(DQMKey const& other) const;
+  };
+  
+  inline bool DQMKey::operator<(DQMKey const& other) const
+  {
+    if ( runNumber != other.runNumber) return runNumber < other.runNumber;
+    if ( lumiSection != other.lumiSection) return lumiSection < other.lumiSection;
+    return updateNumber < other.updateNumber;
+  }
+  
+  inline bool DQMKey::operator==(DQMKey const& other) const
+  {
+    return (runNumber == other.runNumber &&
+      lumiSection == other.lumiSection &&
+      updateNumber == other.updateNumber);
+  }
+
+} // namespace stor
+
+#endif // StorageManager_DQMKey_h 
+
+
+/// emacs configuration
+/// Local Variables: -
+/// mode: c++ -
+/// c-basic-offset: 2 -
+/// indent-tabs-mode: nil -
+/// End: -
