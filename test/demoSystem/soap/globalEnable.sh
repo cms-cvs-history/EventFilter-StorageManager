c2b9d2814371ece9278874251fd5306f91c18f73
Kurt Biery <sha1-a0b81a63ebfa22b3eb476206725e29d6b356d7db@cern.ch>
2009-07-23 18:01:09 +0000

    Moved setting of run numbers from startupSystem script to globalEnable script.

--- test/demoSystem/soap/globalEnable.sh
+++ test/demoSystem/soap/globalEnable.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+echo ""
+echo "========================================"
+echo "Setting run numbers..."
+echo "========================================"
+./setRunNumbers.sh
+echo ""
+
 ./sendCmdToAllApps.sh Enable
--- test/demoSystem/soap/startupSystem.sh
+++ test/demoSystem/soap/startupSystem.sh
@@ -2,11 +2,4 @@
 
 ./globalConfigure.sh
 
-echo ""
-echo "========================================"
-echo "Setting run numbers..."
-echo "========================================"
-./setRunNumbers.sh
-echo ""
-
 ./globalEnable.sh
