0dd0da3d1ebb006bcca6500e662314395b65c401
cmsbuild <>
2010-06-25 11:05:37 +0000

    This commit was manufactured by cvs2git to create tag 'EventFilter- StorageManager-CMSSW_3_6_3_patch1'.
    Sprout from EventFilter-StorageManager-branch_3_5_x 2010-04-28 13:06:58 UTC cmsbuild 'This commit was manufactured by cvs2git to create branch 'EventFilter-'
    Cherrypick from EventFilter-StorageManager-branch_3_5_x 2010-06-25 11:05:36 UTC Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch> 'Prepare for branch tag V05-10-03.':
        ChangeLog
        src/DbFileHandler.cc

--- src/DbFileHandler.cc
+++ src/DbFileHandler.cc
@@ -1,4 +1,4 @@
-// $Id: DbFileHandler.cc,v 1.5 2010/04/12 12:05:01 mommsen Exp $
+// $Id: DbFileHandler.cc,v 1.6 2010/04/28 13:06:10 mommsen Exp $
 /// @file: DbFileHandler.cc
 
 #include <EventFilter/StorageManager/interface/DbFileHandler.h>
@@ -79,7 +79,10 @@ void DbFileHandler::addReportHeader
 ) const
 {
   msg << "Timestamp:" << static_cast<int>(timestamp)
-    << "\trun:" << _runNumber << "\t";
+    << "\trun:" << _runNumber
+    << "\thost:" << _dwParams._hostName
+    << "\tinstance:" << _dwParams._smInstanceString
+    << "\t";
 }
 
 
