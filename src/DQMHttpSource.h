37db5d047cd877c4e2b7f1d295c820ab387ede5b
cmsbuild <>
2011-04-20 08:01:35 +0000

    This commit was manufactured by cvs2git to create tag 'EventFilter- StorageManager-CMSSW_4_1_7_patch3'.
    Sprout from master 2011-04-20 08:01:28 UTC Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch> 'Prepare for tag V07-04-00.'
    Cherrypick from master 2010-08-06 20:24:33 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Remove using directives in headers':
        test/DQMMessageAnalyzer.cc
        test/TestHelper.h
    Cherrypick from EventFilter-StorageManager-branch_3_10_x 2011-04-18 15:18:58 UTC Remi Mommsen <sha1-09ace11b042676bf0011c424f6ae14c5eceb9645@cern.ch> 'Reduce differences btw branch_3_10_x and trunk to bare minimum.':
        interface/DQMTopLevelFolder.h
        interface/I2OChain.h
        src/ChainData.cc
        src/ChainData.h
        src/ConsumerUtils.icc
        src/DQMHttpSource.cc
        src/DQMHttpSource.h
        src/DQMTopLevelFolder.cc
        src/EventMsgData.cc
        src/EventServerProxy.icc
        src/EventStreamHttpReader.cc
        src/EventStreamHttpReader.h
        src/I2OChain.cc
        test/I2OChain_t.cpp
    Delete:
        scripts/inject/InjectWorker.pl
        scripts/inject/NotifyWorker.pl
        scripts/inject/compat/closeFile.pl
        scripts/inject/compat/insertFile.pl
        scripts/inject/compat/notifyTier0.pl
        scripts/inject/compat/sm_compat.sh
        scripts/inject/injectFileIntoTransferSystem.pl
        scripts/inject/log4perl.conf
        scripts/inject/sendNotification.sh
        scripts/inject/sm_hookscript.pl
        scripts/inject/sm_nfscopy.sh
        scripts/inject/t0inject.sh
        scripts/inject/t0notify.sh
        scripts/inject/t0test.sh
        scripts/operations/check_copyworker.sh
        scripts/operations/cleanupEmu.sh
        scripts/operations/cleanupLookarea.sh
        scripts/operations/cleanupOrphans.cfg
        scripts/operations/cleanupOrphans.pl
        scripts/operations/cleanupOrphans_hosts.cfg
        scripts/operations/generateOrphansReport.pl
        scripts/operations/getwwpn.sh
        scripts/operations/injbyhand.sh
        scripts/operations/makeall.sh
        scripts/operations/mk_satamap.pl
        scripts/operations/mkstore.sh
        scripts/operations/mkvol.sh
        scripts/operations/monitoringSar.sh
        scripts/operations/setup_sm.sh
        scripts/operations/smCleanupFiles.pl
        scripts/operations/smorphans.cfg
        scripts/operations/smorphans.sh
        scripts/operations/smorphans_hosts.cfg
        scripts/sql/CopyFilesInfoToSMSummary.pl
        scripts/sql/History.txt
        scripts/sql/add_rights_example.sql
        scripts/sql/createEoLSTables.sql
        scripts/sql/createInjectionTables.sql
        scripts/sql/createInstanceTable.sql
        scripts/sql/createOrphansTable.sql
        scripts/sql/createSMSummaryTable.sql
        scripts/sql/createStoredProcs.sql
        scripts/sql/createTriggers.sql
        scripts/sql/grant_rights_example.sql
        scripts/sql/rate_averageFunc.sql
        scripts/sql/rate_todisk_averageFunc.sql
        scripts/sql/rate_tot0_averageFunc.sql
        scripts/sql/time_diff.sql
        scripts/sql/tochardsFunc.sql
        scripts/sql/view_closed.sql
        scripts/sql/view_instSummary.sql
        scripts/sql/view_instances.sql
        scripts/sql/view_last.sql
        scripts/sql/view_last_run.sql
        scripts/sql/view_last_runs.sql
        scripts/sql/view_open.sql
        scripts/sql/view_streams.sql
        scripts/sql/view_streams_full.sql
        scripts/sql/view_summary.sql
        scripts/test/injection_test/build_files.sh
        scripts/test/injection_test/host_list.cfg
        scripts/test/injection_test/submit_files.sh
        scripts/test/injection_test/transtest.sh
        scripts/test/injection_test/txtst.cfg

--- src/DQMHttpSource.h
+++ src/DQMHttpSource.h
@@ -1,4 +1,4 @@
-// $Id: DQMHttpSource.h,v 1.12 2011/03/30 15:16:48 mommsen Exp $
+// $Id: DQMHttpSource.h,v 1.9.18.1 2011/03/07 11:33:04 mommsen Exp $
 /// @file: DQMHttpSource.h
 
 #ifndef StorageManager_DQMHttpSource_h
@@ -25,8 +25,8 @@ namespace edm
     the StorageManager or SMProxyServer to get DQM (histogram) data.
     
     $Author: mommsen $
-    $Revision: 1.12 $
-    $Date: 2011/03/30 15:16:48 $
+    $Revision: 1.9.18.1 $
+    $Date: 2011/03/07 11:33:04 $
   */
 
   class DQMHttpSource : public edm::RawInputSource
@@ -51,8 +51,8 @@ namespace edm
     virtual std::auto_ptr<edm::Event> readOneEvent();
     void initializeDQMStore();
 
-    stor::EventServerProxy<stor::DQMEventConsumerRegistrationInfo> dqmEventServerProxy_;
     DQMStore* dqmStore_;
+    stor::EventServerProxy<stor::DQMEventConsumerRegistrationInfo> dqmEventServerProxy_;
 
     static boost::mutex mutex_;
   };
